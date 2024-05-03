054fa061168147d5a9f6308ea9a16c74ccbbc809
Rubén Waldo <ruben.waldo6703@alumnos.udg.mx>
2024-05-03 17:05:54 -0600

    Initial commit

---
+++ src/main/java/com/example/PizzaAWS/PizzaAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.PizzaAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaAwsApplication.class, args);
+	}
+
+}
